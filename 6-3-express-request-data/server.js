6141ca45e81bcbe583e72ca891c0574c70b92a4f
ALJADDAWI <s202248800@ccm.kfupm.edu.sa>
2025-11-12 09:14:36 +0300

    task 1 complete

--- 6-3-express-request-data/server.js
+++ 6-3-express-request-data/server.js
@@ -107,7 +107,7 @@ LAB SETUP INSTRUCTIONS
 import express from "express";
 const app = express();
 
-
+app.listen(3000, ()=> console.log("API running at http://localhost:3000"));
 // create server
 
 
